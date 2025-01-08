9d5441ac9a798ee8b4308759b6ce774565e9cec3
Troy Zhang <troyz0213@gmail.com>
2025-01-08 11:47:12 -0800

    git push

--- saas/app/page.tsx
+++ saas/app/page.tsx
@@ -2,33 +2,46 @@ import { ContainerScroll } from "@/components/global/container-scroll-animation"
 import Navbar from "../components/global/navbar";
 import Image from "next/image";
 import { Button } from '../components/ui/button'
+import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
+import { clients } from "@/lib/constants";
 export default function Home() {
   return (
-  <main>
-    <Navbar />
-    <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">
-      <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
-      <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
-        <ContainerScroll
-        titleComponent={
-          <div className='flex items-center justify-center flex-col'>
-            <Button
-              size={'lg'}
-              className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
-            >
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
-                Start For Free Today
-              </span>
-            </Button>
-            <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
-              Automate Your Work With Fuzzie
-            </h1>
-          </div>
-        }
-        ></ContainerScroll>
-      </div>
-    </section>
-  </main>
+      <main>
+      <Navbar />
+      <section className="h-screen w-full bg-neutral-950 rounded-md relative flex flex-col items-center antialiased">
+        <div className="absolute inset-0 h-full w-full items-center px-5 py-24 background-radial-gradient"></div>
+        <div className="h-[700px] flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center justify-center flex-col">
+                <Button
+                  size={"lg"}
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600 md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black group-hover:to-black">
+                    Start For Free Today
+                  </span>
+                </Button>
+                <h1 className="text-5xl md:text-8xl bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Automate Your Work With Fuzzie
+                </h1>
+              </div>
+            }
+          />
+        </div>
+      </section>
+      <InfiniteMovingCards
+        className="md:mt-[18rem] mt-10"
+        items={clients}
+        direction="right"
+        speed="slow"
+      />
+      <section>
+        <HeroParallas products={products}></HeroParallas>
+      </section>
+
+    </main>
+
   )
 
 }
--- saas/lib/constants.ts
+++ saas/lib/constants.ts
@@ -1,3 +1,3 @@
 export const clients = [...new Array(10)].map((client, index) => ({ 
-    href: '/${index + 1}.png',
+    href: `/${index + 1}.png`,
 }))
